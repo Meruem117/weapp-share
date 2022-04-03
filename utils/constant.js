8f01fe42ff7969de93c65e4c7024ebeb542c322b
Meruem117 <1572677319@qq.com>
2022-04-04 02:45:20 +0800

    update user

--- utils/constant.js
+++ utils/constant.js
@@ -1,10 +1,4 @@
 export const GENDER = {
-  MALE: {
-    value: 1,
-    image: '/images/icons/male.png'
-  },
-  FEMALE: {
-    value: 0,
-    image: '/images/icons/female.png'
-  }
+  MALE: 1,
+  FEMALE: 0
 }
